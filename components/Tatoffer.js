c9b7208a2ee2acba9d60bc15e75b3f4827c4a541
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2025-02-18 15:17:23 +0530

    new

--- components/Tatoffer.js
+++ components/Tatoffer.js
@@ -87,7 +87,7 @@ const NewsEvents = () => {
       title: "Innovative Research",
       description:
         "A hub for pioneering research with state-of-the-art equipment and expert guidance.",
-      img: "/images/research.jpg",
+      img: "/images/research14.jpg",
       bgColor: "bg-pink-600",
       borderColor: "border-pink-600",
     },
